4de8be2a5615bdd452b4fa6c5a7826eaa7755244
Mayur Suhasiya <mayursuhasiya136@gmail.com>
2023-06-07 15:07:59 +0530

    Revert "added env file"
    This reverts commit 46affaa2313ba4c9f2cdfb13e8f1bf29fc38b436.

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
-require("dotenv").config();
-
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = process.env.MONGODB_URL;
+const uri =
+  "mongodb+srv://mayursuhasiya:mayur123@cluster0.0wbcu84.mongodb.net/?retryWrites=true&w=majority";
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
